c0fcdf29325489a7226e7c7b85bb04731aa885be
Prashant Dubey <pdubey1924@gmail.com>
2025-08-31 16:48:21 +0530

    done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3001", "http://127.0.0.1:3001"], 
+    origin: ["http://localhost:3001"], 
     methods: ['GET', 'POST'],
     credentials: true,
   },
